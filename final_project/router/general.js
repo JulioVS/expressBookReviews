9fc34e20a10dffbe1384a40711e25d1805364f11
JulioVS <julioer@me.com>
2024-05-13 14:18:54 -0400

    Add Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,8 +22,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let index = 0;
+  let selected = [];
+  
+  for (index in books) {
+    if (books[index]['author'] == author) {
+        selected.push(books[index])  
+    }
+  };
+  return res.send(selected);
 });
 
 // Get all books based on title
