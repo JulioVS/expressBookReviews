a591c308c6e4f1b52619f5c1be206f7dd6ee5d85
JulioVS <julioer@me.com>
2024-05-13 18:34:13 -0400

    Add Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -48,6 +48,15 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.send(books[isbn]);
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization['username'];
+
+    delete books[isbn]['reviews'][username]
+    return res.send(books[isbn]);
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
