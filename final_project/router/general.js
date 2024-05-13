cb94a6e245780db5743d670b56ce16bac47a22ed
JulioVS <julioer@me.com>
2024-05-13 13:57:57 -0400

    add Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,16 +12,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   return res.send(JSON.stringify(books,null,4))
-  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  return res.send(books[req.params.isbn])
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
